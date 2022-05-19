81babd6b19b2b40f4cd57d5ae3e3874ebfdd6f7e
chamanthimuppala963 <chamanthimuppala963@gmail.com>
2022-05-19 15:11:38 +0530

    modified

--- DBUtils.java
+++ DBUtils.java
@@ -6,5 +6,10 @@ Hello Iam updating this..
 Hello iam updating in dev branch..
 This file contains the DBUtils.java
 this is for pull
+
 this line for merge
+<<<<<<< HEAD
 pull line
+=======
+not for cherry pick
+>>>>>>> 0ef9e34 (not for cherry pick)
